3c3dc908ed317c217a260faff5b03ab3f26733ae
Timothy Natal <timothynatal@pursuit.org>
2025-01-28 14:08:37 -0500

    added css file for App.jsx

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import "./App.css"
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<h1>Hello</h1>} />
+      <Route path="/" element={
+        <div className='page-wrap'>
+          <h1>Hello</h1>
+        </div>
+      } />
     </Routes>
   )
 }
