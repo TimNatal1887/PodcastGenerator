59741f1311a5ec1b9bce7a25ffe78e9d4ae31e7b
Timothy Natal <timothynatal@pursuit.org>
2025-01-30 13:39:56 -0500

    Setup backend environment

---
+++ BackEnd/app.js
@@ -0,0 +1,30 @@
+// DEPENDENCIES
+const cors = require("cors");
+const express = require("express");
+
+
+// CONFIGURATION
+const app = express();
+
+// MIDDLEWARE
+app.use(cors());
+
+app.use((req, _res, next) => {
+  console.log("Origin Requested:", req.headers.origin);
+  next();
+});
+
+app.use(express.json());
+
+// ROUTES
+app.get("/", (_req, res) => {
+  res.send("Welcome to Firebase Backend Server");
+});
+
+// 404 PAGE
+app.get("*", (_req, res) => {
+  res.status(404).send("Page not found");
+});
+
+// EXPORT
+module.exports = app;
